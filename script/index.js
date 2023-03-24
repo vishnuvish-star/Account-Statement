127543229c3b10bf63769e792917757586cbf119
vishnu <vishnu981203@gmail.com>
2023-03-24 02:25:22 -0400

    Getting Elements

---
+++ script/index.js
@@ -0,0 +1,5 @@
+"use strict";
+const transactionBoxEl = document.getElementById("transaction-box");
+const amountEl = document.getElementById("transaction-amount-box");
+const buttonEl = document.getElementById("button");
+// console.log(transactionBoxEl, amountEl, buttonEl);
